c8b84804f7e8ffbbe7350941ba412b9b9ee96d5c
kmakantasis <konst.makantasis@gmail.com>
2014-05-02 19:42:56 +0300

    Slight modifications to include pre-computation of pairwise distances

--- InterClusterCost.m
+++ InterClusterCost.m
@@ -1,19 +1,29 @@
-function cost = InterClusterCost(data, solution, num_classes)
+function cost = InterClusterCost(cost_matrix, solution, num_classes)
 
 cost = 0;
 for i=1:num_classes
     idx = find(solution==i);
-    class_i = data(idx, 1:2);
-    [sz, ret] = size(class_i);
-    for j=1:sz
-        point1 = class_i(j,:);
-        for l=j:sz
-            point2 = class_i(l,:);
-            cost = cost + norm(point1 - point2);
+    for j=1:length(idx)
+        for l=j:length(idx)
+            cost = cost + cost_matrix(idx(j),idx(l));
         end
     end
-            
 end
+        
+% cost = 0;
+% for i=1:num_classes
+%     idx = find(solution==i);
+%     class_i = solution(idx, 1:2);
+%     [sz, ret] = size(class_i);
+%     for j=1:sz
+%         point1 = class_i(j,:);
+%         for l=j:sz
+%             point2 = class_i(l,:);
+%             cost = cost + norm(point1 - point2);
+%         end
+%     end
+%             
+% end
 
 
 end
