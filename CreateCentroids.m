b49bce0ce315512e48aa952b70b20269c5f3fca7
kostas <konst.makantasis@gmail.com>
2014-07-28 19:36:41 +0300

    Remove junk files

--- CreateCentroids.m
+++
@@ -1,8 +0,0 @@
-function c = CreateCentroids(data, num_classes, solution)
-    c = [];
-    for i=1:num_classes
-        idx = find(solution==i);
-        c = [c; mean(data(idx,1:2))];
-    end
-end
-
